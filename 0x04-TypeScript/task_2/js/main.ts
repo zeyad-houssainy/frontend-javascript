7bbc91dd15a8be8b716a2f296a43f530d0c31662
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-11-10 00:44:01 +0300

    0. Creating an interface for a student

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,10 +44,8 @@ class Teacher implements TeacherInterface {
 
 // createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number') {
-    if (salary < 500) {
-      return new Teacher();
-    }
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
   }
   return new Director();
 }
